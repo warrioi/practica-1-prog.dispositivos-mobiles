11ae6558522701fbcf879be677fdbad20d7ef7f4
Cristhian Daniel Rojas <rojasdani248@gmail.com>
2020-09-04 21:13:49 -0400

    hola mundo

---
+++ app/src/main/java/com/warrior/holamundo/HolaMundo.java
@@ -0,0 +1,15 @@
+package com.warrior.holamundo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HolaMundo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.hola_mundo);
+    }
+}
